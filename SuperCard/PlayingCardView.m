466d4c2e2c1a39f439d94207839cb7a935c8b65c
ceoofufo <ceoofufo@ceoofufodeMacBook-Air.local>
2015-07-07 09:53:23 +0800

    添加人头牌

--- SuperCard/PlayingCardView.m
+++ SuperCard/PlayingCardView.m
@@ -8,10 +8,29 @@
 
 #import "PlayingCardView.h"
 
+@interface PlayingCardView()
+@property (nonatomic) CGFloat faceCardScaleFactor;
+@end
+
 @implementation PlayingCardView
 
 #define CORNER_FONT_STANDARD_HEIGHT 180.0
 #define CORNER_RADIUS 12.0
+#define DEFAULT_FACE_CARD_SCALE_FACTOR 0.90
+
+@synthesize faceCardScaleFactor = _faceCardScaleFactor;
+
+- (CGFloat) faceCardScaleFactor
+{
+    if (! _faceCardScaleFactor) _faceCardScaleFactor = DEFAULT_FACE_CARD_SCALE_FACTOR;
+    return _faceCardScaleFactor;
+}
+
+-(void) setFaceCardScaleFactor:(CGFloat)faceCardScaleFactor
+{
+    _faceCardScaleFactor = faceCardScaleFactor;
+    [self setNeedsDisplay];
+}
 
 -(CGFloat) cornerScaleFactor
 {
@@ -57,6 +76,50 @@
     
     [[UIColor blackColor] setStroke];
     [roundedRect stroke];
+    
+    UIImage *faceImage = [UIImage imageNamed:[NSString stringWithFormat:@"%@%@.jpg",[self rankAsString], self.suit]];
+    if (faceImage) {
+        CGRect imageRect = CGRectInset(self.bounds, self.bounds.size.width * (1.0 - self.faceCardScaleFactor),
+                                       self.bounds.size.height * (1.0 - self.faceCardScaleFactor));
+        [faceImage drawInRect:imageRect];
+    } else {
+        [self drawPips];
+    }
+    
+    [self drawCorners];
+}
+
+-(void)drawPips
+{
+    
+}
+
+-(NSString *)rankAsString
+{
+    return @[@"?",@"A",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"J",@"Q",@"K"][self.rank];
+}
+
+- (void) drawCorners
+{
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    paragraphStyle.alignment = NSTextAlignmentCenter;
+    
+    UIFont *cornerFont = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    cornerFont = [cornerFont fontWithSize:cornerFont.pointSize * [self cornerScaleFactor]];
+    
+    NSAttributedString *cornerText = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%@\n%@",[self rankAsString],self.suit] attributes:@{NSFontAttributeName : cornerFont, NSParagraphStyleAttributeName : paragraphStyle}];
+    
+    CGRect textBounds;
+    textBounds.origin = CGPointMake([self cornerOffset], [self cornerOffset]);
+    textBounds.size = [cornerText size];
+    
+    [cornerText drawInRect:textBounds];
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextTranslateCTM(context, self.bounds.size.width, self.bounds.size.height);
+    CGContextRotateCTM(context, M_PI);
+    [cornerText drawInRect:textBounds];
+    
 }
 
 #pragma mark - initialization
